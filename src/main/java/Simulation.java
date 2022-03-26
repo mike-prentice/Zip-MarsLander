bd129ba001c4c0156e7eb17b8e28ea8f3ff5c9a4
Mike <29170001+mike-prentice@users.noreply.github.com>
2022-03-25 21:30:34 -0400

    simulation working, 3/4 tests pass

--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -73,6 +73,10 @@ public class Simulation {
     }
 
     public static void main(String[] args) {
+        Vehicle v = new Vehicle(randomaltitude());
+        Simulation simulation = new Simulation(v);
+        BurnInputStream b = new BurnInputStream();
+        simulation.runSimulation(b);
         // create a new Simulation object with a random starting altitude
         // create a new BurnInputStream
         // pass the new BurnInputStream to the runSimulation method
