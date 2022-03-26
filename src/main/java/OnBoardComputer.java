150eec2ab1c3d9bc3797512d86930c1baef16d94
Mike <29170001+mike-prentice@users.noreply.github.com>
2022-03-25 23:15:30 -0400

    computer sim part1

--- src/main/java/OnBoardComputer.java
+++ src/main/java/OnBoardComputer.java
@@ -3,7 +3,16 @@ public class OnBoardComputer implements BurnStream {
     @Override
     public int getNextBurn(DescentEvent status) {
         int burn = 0;
-        
+        while (status.Altitude > 0){
+            if (status.Altitude>=200){
+                burn = 200;
+            } else if (status.Altitude < 200 && status.Altitude>= 100){
+                burn = 200;
+            } else {
+                burn = (status.Velocity * status.Velocity)/(2 * status.Altitude + 200);
+            }
+        }
+
         System.out.println(burn); /*hack!*/
         return burn;
     }
