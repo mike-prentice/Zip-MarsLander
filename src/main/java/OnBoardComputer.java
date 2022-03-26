52fd9b1cf55f1840b510f5d0892d6d269dbdebe6
Mike <29170001+mike-prentice@users.noreply.github.com>
2022-03-25 23:37:47 -0400

    we did it

--- src/main/java/OnBoardComputer.java
+++ src/main/java/OnBoardComputer.java
@@ -2,16 +2,11 @@ public class OnBoardComputer implements BurnStream {
 
     @Override
     public int getNextBurn(DescentEvent status) {
-        int burn = 0;
-        while (status.Altitude > 0){
-            if (status.Altitude>=200){
-                burn = 200;
-            } else if (status.Altitude < 200 && status.Altitude>= 100){
-                burn = 200;
-            } else {
-                burn = (status.Velocity * status.Velocity)/(2 * status.Altitude + 200);
-            }
-        }
+        int v = status.Velocity;
+        int a = status.Altitude;
+        int burn = (v * v) / (2 * a) + 99;
+
+
 
         System.out.println(burn); /*hack!*/
         return burn;
